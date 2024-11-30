2e6f86b9ca14d7318160a88cf53ebf092c2faa05
Eunkyu Seong <uocup96@gmail.com>
2024-11-30 15:09:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 import streamlit as st
-from IPython.core.display import display_html
-with open("./home.md","r") as home:
-    home_content = home.read()
 
-st.markdown(home_content,unsafe_allow_html=True)
 
 view = [100,150,30]
 st.write('Youtube view')
